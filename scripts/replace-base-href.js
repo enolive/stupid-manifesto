6dabeac65aa5618d1ea320fab6e50a88a7b3f88e
Christoph Welcz <christoph.welcz@codecentric.de>
2025-08-06 15:25:38 +0200

    💚 some better logging

--- scripts/replace-base-href.js
+++ scripts/replace-base-href.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const baseHref = process.env.BASE_HREF || '';
 const filePath = path.join(__dirname, '../public/index.html');
+console.log(`replacing base href in ${filePath} with ${baseHref}`)
 
 let content = fs.readFileSync(filePath, 'utf8');
 content = content.replace(/{BASE_HREF}/g, baseHref);
